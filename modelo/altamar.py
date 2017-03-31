aace7a488cecb5523bbbe4f049c233f7a1d986ea
patriciadcl <patriciadcl@gmail.com>
2017-03-31 23:34:52 +0200

    add-altamar-modelo

---
+++ modelo/altamar.py
@@ -0,0 +1,49 @@
+class Zonas:
+    def __init__(self,id_aemet,id_api,texto):
+        self.__id_aemet = id_aemet
+        self.__id_api = id_api
+        self.__texto = texto
+
+    @property
+    def id_aemet(self):
+        return self.__id_aemet
+
+    @property
+    def id_api(self):
+        return self.__id_api
+
+    @property
+    def texto(self):
+        return self.__texto
+
+    @property
+    def to_dict(self):
+        diccionario = dict()
+        for k, v in self.__dict__.items():
+            key = k.split("__")[1]
+            diccionario[key] = v
+        return diccionario
+
+
+class SubZonas:
+
+    def __init__(self,id_aemet,texto):
+        self.__id_aemet = id_aemet
+        self.__texto = texto
+
+    @property
+    def id_aemet(self):
+        return self.__id_aemet
+
+    @property
+    def texto(self):
+        return self.__texto
+
+    @property
+    def to_dict(self):
+        diccionario = dict()
+        for k, v in self.__dict__.items():
+            key = k.split("__")[1]
+            diccionario[key] = v
+        return diccionario
+
