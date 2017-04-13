1e30fb2d049e73340eb71bc7b3db31ab9e09eefb
patriciadcl <patriciadcl@gmail.com>
2017-04-13 15:06:19 +0200

    corregido-error-main

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ aemet_api = api.AemetAPI()
 
 base_dir = os.path.dirname(os.path.realpath('__file__'))
 
-util = None
+utils = None
 
 meteoserver_ddbb = None
 
@@ -86,6 +86,6 @@ def playa(id_playa):
 
 
 if __name__ == '__main__':
-    meteoserver_ddbb = BaseDatos(update=True)
+    meteoserver_ddbb = BaseDatos(update=False)
     utils = servidor.ServidorUtils(base_dir, meteoserver_ddbb)
     app.run(debug=True)
